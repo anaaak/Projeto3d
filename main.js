f63b2abe71f6e729ba7b8b711c74275edd01f859
ana ketllyn <105050863+anaaak@users.noreply.github.com>
2024-03-12 10:10:44 -0300

    22222

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
 
 for (let i = 0; i 1 < botoes.length; j++){
-    
+    botoes[i].onclick = function (){
+        botoes[i].classList.add("ativo")
+    }
 }
 
