8610ce7c39a74aa4d796a06593c4f6329ab85ba5
ana ketllyn <105050863+anaaak@users.noreply.github.com>
2024-04-30 11:14:20 -0300

    jnrgbghaebhrb

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ for (let i = 0; i<botoes.length; i++) {
     }
 }
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo2 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo3 = new Date("2024-12-28T23:59:59");
-const tempoObjetivo4 = new Date("2024-12-28T23:59:59");
+const tempoObjetivo1 = new Date("2024-12-10T23:59:59");
+const tempoObjetivo2 = new Date("2024-12-09T23:59:59");
+const tempoObjetivo3 = new Date("2024-12-08T23:59:59");
+const tempoObjetivo4 = new Date("2024-12-07T23:59:59");
 
 const tempos = [
     tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4
