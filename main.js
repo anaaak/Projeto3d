eecab8a99c286b42a3f64b3df5e329047d7af316
ana ketllyn <105050863+anaaak@users.noreply.github.com>
2024-04-30 11:10:59 -0300

    .

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function calculaTempo(tempoObjetivo1){
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
 
-        segundos %=60;
+        segundos %= 60;
         minutos %= 60;
         horas %= 24;
     
